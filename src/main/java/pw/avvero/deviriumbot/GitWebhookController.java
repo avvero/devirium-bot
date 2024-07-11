587fe37204acd72ff04afaef11da8f29fc255410
belyaev-ay <anton.belyaev@sibit.tech>
2024-07-11 19:16:55 +0700

    Support of markdownv2

--- src/main/java/pw/avvero/deviriumbot/GitWebhookController.java
+++ src/main/java/pw/avvero/deviriumbot/GitWebhookController.java
@@ -9,11 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static java.lang.String.format;
 
@@ -22,18 +18,18 @@ import static java.lang.String.format;
 public class GitWebhookController {
 
     private final TelegramService telegramService;
+    private final TelegramMessageMapper mapper;
     private final String deviriumChatId;
     private final String gardenerChatId;
-    private final String deviriumLink;
 
     public GitWebhookController(TelegramService telegramService,
+                                TelegramMessageMapper mapper,
                                 @Value("${devirium.chatId}") String deviriumChatId,
-                                @Value("${devirium.gardenerChatId}") String gardenerChatId,
-                                @Value("${devirium.link}") String deviriumLink) {
+                                @Value("${devirium.gardenerChatId}") String gardenerChatId) {
         this.telegramService = telegramService;
+        this.mapper = mapper;
         this.deviriumChatId = deviriumChatId;
         this.gardenerChatId = gardenerChatId;
-        this.deviriumLink = deviriumLink;
     }
 
     @PostMapping("/git/webhook")
@@ -42,39 +38,8 @@ public class GitWebhookController {
             log.debug("Note {} would be ignored because of #draft tag", request.file);
             return;
         }
-        String content = request.content;
-        Map<String, String> meta = new HashMap<>();
-        //
-        Pattern pattern = Pattern.compile("\\[\\[.*\\]\\]");
-        Matcher matcher = pattern.matcher(content);
-        while (matcher.find()) {
-            String id = UUID.randomUUID().toString().replace("-", "");
-            meta.put(id, matcher.group());
-            content = content.replace(matcher.group(), id);
-        }
-        // exclude: '>', '`'
-        for (char ch : new char[]{'_', '*', '~', '#', '+', '-', '=', '|', '{', '}', '.', '!', '[', ']', '(', ')'}) {
-            content = content.replace("" + ch, "\\" + ch);
-        }
-        //
-        for (Map.Entry<String, String> entry : meta.entrySet()) {
-            content = content.replace(entry.getKey(), entry.getValue());
-        }
-        // links
-        if (request.links != null) {
-            for (Map.Entry<String, String> link : request.links.entrySet()) {
-                String url = format("[%s](%s/%s)", link.getKey(), deviriumLink, link.getValue());
-                content = content.replace(format("[[%s]]", link.getKey()), url);
-            }
-        }
-        // Check unresolved links
-        Matcher matcherUnresolvedLink = Pattern.compile("\\[\\[.*?\\]\\]").matcher(content);
-        if (matcherUnresolvedLink.find()) {
-            telegramService.sendMessage(gardenerChatId, null, format("Can't process %s: Can't resolve link %s",
-                    request.file, matcherUnresolvedLink.group()), null);
-            return;
-        }
         try {
+            String content = mapper.map(request.content, request.links);
             telegramService.sendMessage(deviriumChatId, null, content, "MarkdownV2");
         } catch (Exception e) {
             telegramService.sendMessage(gardenerChatId, null, format("Can't process %s: %s", request.file, e.getMessage()), null);
---
+++ src/main/java/pw/avvero/deviriumbot/TelegramMessageMapper.java
@@ -0,0 +1,64 @@
+package pw.avvero.deviriumbot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.String.format;
+
+@Component
+public class TelegramMessageMapper {
+
+    public static final Pattern ZET_LINK = Pattern.compile("\\[\\[.*\\]\\]");
+    public static final Pattern MD_LINK = Pattern.compile("\\[([^\\]]+)\\]\\(([^\\)]+)\\)");
+
+    private final String deviriumLink;
+
+    public TelegramMessageMapper(@Value("${devirium.link}") String deviriumLink) {
+        this.deviriumLink = deviriumLink;
+    }
+
+    public String map(String content, Map<String, String> links) throws Exception {
+        Map<String, String> meta = new HashMap<>();
+        // Extract meta
+        content = extractMeta(content, meta, ZET_LINK, MD_LINK);
+        // Escape characters except: '>', '`'
+        for (char ch : new char[]{'_', '*', '~', '#', '+', '-', '=', '|', '{', '}', '.', '!', '[', ']', '(', ')'}) {
+            content = content.replace("" + ch, "\\" + ch);
+        }
+        // Enrich meta
+        for (Map.Entry<String, String> entry : meta.entrySet()) {
+            content = content.replace(entry.getKey(), entry.getValue());
+        }
+        // Update links with real ones
+        if (links != null) {
+            for (Map.Entry<String, String> link : links.entrySet()) {
+                String url = format("[%s](%s/%s)", link.getKey(), deviriumLink, link.getValue());
+                content = content.replace(format("[[%s]]", link.getKey()), url);
+            }
+        }
+        // Check unresolved links
+        Matcher matcherUnresolvedLink = ZET_LINK.matcher(content);
+        if (matcherUnresolvedLink.find()) {
+            throw new Exception(format("Can't resolve link %s", matcherUnresolvedLink.group()));
+        }
+        return content;
+    }
+
+    private String extractMeta(String content, Map<String, String> meta, Pattern... patterns) {
+        for (Pattern pattern : patterns) {
+            Matcher matcher = pattern.matcher(content);
+            while (matcher.find()) {
+                String id = UUID.randomUUID().toString().replace("-", "");
+                meta.put(id, matcher.group());
+                content = content.replace(matcher.group(), id);
+            }
+        }
+        return content;
+    }
+}
