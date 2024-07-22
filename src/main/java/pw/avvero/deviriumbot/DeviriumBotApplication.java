e703207d27fe56eac3e48812b1a0460f36d255e7
belyaev-ay <anton.belyaev@sibit.tech>
2024-07-22 12:49:29 +0700

    Publish command

--- src/main/java/pw/avvero/deviriumbot/DeviriumBotApplication.java
+++ src/main/java/pw/avvero/deviriumbot/DeviriumBotApplication.java
@@ -1,7 +1,11 @@
 package pw.avvero.deviriumbot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DeviriumBotApplication {
@@ -10,4 +14,24 @@ public class DeviriumBotApplication {
         SpringApplication.run(DeviriumBotApplication.class, args);
     }
 
+    @Component
+    public class StartupEventListener implements ApplicationListener<ContextRefreshedEvent> {
+
+        private final TelegramService telegramService;
+        private final String deviriumChatId;
+        private final String gardenerChatId;
+
+        public StartupEventListener(TelegramService telegramService,
+                                         @Value("${devirium.chatId}") String deviriumChatId,
+                                         @Value("${devirium.gardenerChatId}") String gardenerChatId) {
+            this.telegramService = telegramService;
+            this.deviriumChatId = deviriumChatId;
+            this.gardenerChatId = gardenerChatId;
+        }
+
+        @Override
+        public void onApplicationEvent(ContextRefreshedEvent event) {
+            telegramService.sendMessage(gardenerChatId, null, "Bot is ready to work", "markdown");
+        }
+    }
 }
